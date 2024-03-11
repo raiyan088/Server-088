543dac429b86f79cc807b29fdb011cc6bbd0a936
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-03-12 04:00:25 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,20 +8,22 @@ let mClient = null
 let mTimeout = null
 let mPending = []
 let mUrl = null
+let mPrevJob = ''
 let mJobSolve = 0
 
 let mStart = parseInt(new Date().getTime()/1000)
 let mTime = new Date().toString()
 
-const serviceAccount = JSON.parse(decode('eyJ0eXBlIjoic2VydmljZV9hY2NvdW50IiwicHJvamVjdF9pZCI6ImRhdGFiYXNlMDg4IiwicHJpdmF0ZV9rZXlfaWQiOiI4MjIwMTQxMjA3NWJhZmJjNTQ4NDM0MTM1YzQwYjliNzU5ZTY5NGIyIiwicHJpdmF0ZV9rZXkiOiItLS0tLUJFR0lOIFBSSVZBVEUgS0VZLS0tLS1cbk1JSUV2QUlCQURBTkJna3Foa2lHOXcwQkFRRUZBQVNDQktZd2dnU2lBZ0VBQW9JQkFRQ2RHRk4vdnZhMS9xSFNcbkJxRk0xMnM4UnRJTm1UZHU2YmNkTkhGcGd6K2gza2cySWQ4T1l4bExnYjQyWU5oNVlqelhsOGZSSzJnUnJIMTBcbjRTMzZEOXNuUFBqMHU2U042Uy9XL2ZhRnpWSjlWTU8yMC8rMjdVbG1lUUkwMVhMRWFiUnNJdDdRckhVM3cydVpcbk9FOU9Qd1RqZndRS3lyZVVTdkgzanlyNG9zaktrazBZQkk0bVFBTTlqL2ptYVd6YjVCT3NTVnlOdVJocC9IOFBcbm95OExGb0xwTVhpRG5Xam05ektiK0xiSzVwV1dlZ0dVc3doTytIckU4RzM3WFpzZDdsTitPOEhPV3ZnQ1ZOZlFcbm1CNlgxYWQ5L2FhWWpqQ2RvQnpxUVd2dy9UK3lPM3JIb2JhZnJqMGQzZFlIV2k0Qkl6OXFKS3plY2tsK2NhVkhcbktFSFF3NGVwQWdNQkFBRUNnZ0VBQUk3bThnM2l5TW9GTjI2MldVUnRNVDlEclJCeHI5dFUveDJ4OXZqa1M1NFRcbmRkeklNd0FEWlo4cUJyMndtenJRQWtpYjZVaDlFODMvK3ArM3R2c2lNVmtnUGRkaVdlNDlrU2NoSG4vWXBwRWJcbjZtMEZyRGZhNGtvMUNhekYvZ0J0cDVJMnBqbHNwN1J5TUE3UkNsTkRRRTh4MjV0MWpFbDVZMkt1K1lmempITzNcbjgzeWM2clZ4Z1pqTWF4aVVZR0d3S0gxeDVOeXZNWEZHMnNGdGNCTGdZS0Jsb3NvSUxaY05DR2d1RnhFNXB0WVZcbkhTQmRKSjFHaGNnSnBqR2VtZFozQ3JUV3RkOHdtbHdrUGc4eCtWWG9nVFkzZUs5azN2bE0xb01hYXByMWJ5TUxcbmtyM3hNQVNXZSt3c1ZqTHl5b0J6Skd1OHYwZ1RoZ2tWczFyaS8vVkVVUUtCZ1FEZG45WnFkMXZGVXpMZWVRbjNcbkVxT3U4clB3VFZ4VnRBdTUxSXoxbTZtRlE1aGszdHkra0hHU2VpVThuMWRIWFVneml2YXJPQU9HMDR1U1NaNkNcbjFlUk44bEt1ZzA2QTVXK20xNC90YXlRaldoTmczaWtsYmN5bmY5dTBpZEdqUDRpMHpJQk9vMFh5M3dGazFBd0xcbnErclZXTHdoQnI1YUpEdlVZR2xZV29qRkVRS0JnUUMxZGkrZWxya2grZmZ6WVd1VktLNnhOSmVjanVCWFk2blFcblUxK3FQL0tjRkpuQUI0RmFwRklaR2dqTDd3YkFXSXJFRFZWSVhVT3RZejR2SDhzalVrWEtpNkVwbU84V0xxQ2xcbnNLOGNxbzQ5UGFTS1JJRkovZ2dvUWpNNklOQ1p1Mm1UZ25jWGdZaE9GT29zWEJRRSt6bWhVQkJIcWthaGwyQ1JcbkVoSjdKdys1R1FLQmdDMmxVQW9aajBNekFaVWtwTkxJZWdIOFp1Y2xaOUVYQmhzK0ZkSTRQMC85UGhQNGVzcWRcbkFXZEQxUXVKWGZhSzFlWVh4dm80elRFbHZPZGlWazFvTkYrQmUxb0tIVThhOGtZUXU4b1FlUjcyM0dybEZ1Qm9cbm52QVl1MjJSdlNXOFhqR3V0UFo5VmxqUmFYNFUxbTVJK0ZMOHRiS3ZxdUR4K1lpaVd0QWh4VzJoQW9HQVVXMFhcbk1US3hJRzNxdFd1aHV1em1kdDRDcWwzeWpVUkxqdy9hZmFyMFV0VnJxSisxaTlid2dCTU9uM1JDczAwSDk3R0RcbjgxVG1hWWQrLytaa1lSVXAvQmxyMkI2b0ttd0I2bGUrUEQ3TVZPNjBSdGMyck4weEM5K28zUXljamw5cW9LYUtcbmlpRzE2Rk82UU8yUWppdXRVSlY4dUt2UHhmTUhnaEY1K1lrdHBRRUNnWUIzeE9KaHpUR2U3blpEbTYvZWE4ZWlcbnZqOHpyTzdZV2xDVWF4UERUTmdPZUFWZ29NeTh3WjhGRWJ5aXFFWjJLTFNDazFHbGxvSUZaWHh4c1hwMXZ3blhcbldzaDQ0U1VIdm9FNTIvdTZmMlNlR0ptdXJLT216eGptNEFXb1VVUGpHYWVtU2RSeE5JVGVhTDFTdDQyZ0lLR2NcbkdmR2hoZ3dFZGxqWGZrelE1bDkwREE9PVxuLS0tLS1FTkQgUFJJVkFURSBLRVktLS0tLVxuIiwiY2xpZW50X2VtYWlsIjoiZmlyZWJhc2UtYWRtaW5zZGstaG81bG5AZGF0YWJhc2UwODguaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJjbGllbnRfaWQiOiIxMDYwMTI1NTI3NzU5MjQ2Njg2MDEiLCJhdXRoX3VyaSI6Imh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbS9vL29hdXRoMi9hdXRoIiwidG9rZW5fdXJpIjoiaHR0cHM6Ly9vYXV0aDIuZ29vZ2xlYXBpcy5jb20vdG9rZW4iLCJhdXRoX3Byb3ZpZGVyX3g1MDlfY2VydF91cmwiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9vYXV0aDIvdjEvY2VydHMiLCJjbGllbnRfeDUwOV9jZXJ0X3VybCI6Imh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL3JvYm90L3YxL21ldGFkYXRhL3g1MDkvZmlyZWJhc2UtYWRtaW5zZGstaG81bG4lNDBkYXRhYmFzZTA4OC5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVuaXZlcnNlX2RvbWFpbiI6Imdvb2dsZWFwaXMuY29tIn0='))
+const serviceAccount = JSON.parse(decode('eyJ0eXBlIjoic2VydmljZV9hY2NvdW50IiwicHJvamVjdF9pZCI6ImpvYi1zZXJ2ZXItMDg4IiwicHJpdmF0ZV9rZXlfaWQiOiIzZWU1MjAxMDk5YTRmNTI4YjYzYWNkYjc0OTk1ZDUzYjExZDdhNTkzIiwicHJpdmF0ZV9rZXkiOiItLS0tLUJFR0lOIFBSSVZBVEUgS0VZLS0tLS1cbk1JSUV2QUlCQURBTkJna3Foa2lHOXcwQkFRRUZBQVNDQktZd2dnU2lBZ0VBQW9JQkFRQ05JbjRrQ2MxVmhwSW1cblRiRStiOWFJbjNPVDNmN1Jhd05IMmJ6WXZIVG5ERkdFTGN5Zm91MUdkRHBlSkRBeHpEaGxuRUVBNXphUmN3Q2Rcbjc2ZGwxbFMvRUMwSGRuVEJCY0ZmZDFwWkJCMG9UVlpuZ2gwajFwM3ZQeFdPSW9xZGNMS0w4c3pLUlJkOEdudFBcbkh5SUxjOHdTRFJoazQyS1JJck9qNzdEZlo3T0RuZ0k3VFlZWEdHWkdlZC9LbDgrWHhZbVVvY3FpNGNad05WRldcbkdLSmd5NUxTVUFHQlllb09EYTAxS3Zhb1ozaElxVEV5UzFMc2VFNENUa3drRjBaaFhyZkczdlc5ZGZQNkVhTURcbkl0eUk0bXZ6T24zaHJ6NWJjQmcybkZRRTZjcUpDaGRmbFhKaG81ODBLdlRid25EcWxhT2l0ZlRrNHJUMUVxcE9cbjR2MHV6dkp0QWdNQkFBRUNnZ0VBUlo4NTNicE9KajRqMDZ4bzNyV1ovYnkyN2I5SjZISGpaT3JuQzMzd0oxZytcbjBEY3RwYVJnYTJ5RHJKUXFpQzVIdGV4ZWJyMGdnS0RjTVkwYkpaUVZLMG1tQlBQdEJaazZ1c2JzZFZRZnRCVnVcbnBkSWNZT1VLOVE1SUtsMGt4eVRrbDBBWHdVSlRJd0FIUzFFKzRLcG5oWkliTWcydnZvd0JWVUkxSFFnUm1MKzZcbmtoVFM1ZTUzYnJwZFlFOU04QmhISDVsbTV3OVB2bTJLa1QyejBlV1lXaDJBUUQ5MVBuQTExK2Y3ek96N1FHMFdcbmdxMU5qSjh3TUt3SE5vL0svNEhJQWZSRThDNkROUDJ4UWNkNWdXaXpOSWxnWTF5a2x4RzFRVEo1K3lORDVvVHRcbndKV3RNS1luQkE4cVZSN1R6NVNUVWk1bzlZY3ZML1NsaFozNEJnbWttd0tCZ1FERXBHbGswQ1ZUVFVWVXJEL0FcbjRrR2lGZmI1cEsrODlNL245TnJFWk5zdVEyUzFGQVNIWmtJdFpBN2VIdDY3d3FOWmpJU0RFekk5alZZSnc1akRcbnluMTJhSWNpWHVwQ0RTV3JFNTJOVlZURHE5WGs3WWRDSlZ4Y1pBbmY4ZlVFdEZ1SVpBZ01wVHY0MHMwNXZlSW9cbjEyV1BTRDhGY2ZoTHdvVlVuYnRYVC84Unh3S0JnUUMzdkxqeFhsQmk2YXU3T01sS0gwSzd6RGR1dFpMU2NGb0JcblVDclZxVG8xOGM5ZkJaRmh6Uk5PUHc4YW5kYTdYVHh2elg5Q1JXa0txR0h6cjd6NGNnZDBNd0hQNjlUT25USUxcbmdaOWIzekUrZG1xR2FTZThkRHdUWUxUcHhPejNBWk96Ym1Qek1sNFVRMkNpUnphUzFIZHdBZkF6bVlKV08weWlcbkZWUFY5NUZhS3dLQmdCamhSSVNBNFhnY3VyenYzbEVsVDNDV250MFBQVDBITEpjSW4vVmhYV29KRk9Ea1czNVdcbkxlRllXNWszQnE5eS9RQURpM0NhS1Mwb2lNNUxkVFN3bGhjNU9uL2F5b0Q0OE44b2FETE5yUi9reWZkaEprLzBcbk1pOXVhT0Z3MTdOMHJuQWZWL1Zob3FqazR4cC9ML0pDN3BLbWJYTGU3SytKT1IxdnA1aHdnckZaQW9HQWNEdDNcblBQaS9ZYXdhbW1JMWtuRkY4akRzSzFQK08wMGxyV2Vxd3BoOFZqYysxR3d5UWV0aXY0a0ZVTnpaTGRubDhPVTFcbnR1VnZKSE4yWWNRWFNpdGRJajJGL2R1d1FnVURiTVBnODkyQjF3VytUQnd2aVkzMnBGK09JcjJIZ2RvVXZxWFBcbnA5NDhXV1JPd3RGOUpITmtBYWN0Y2xkeXBmblM5YTdSWndHeVo4RUNnWUFiQkQwZDgzRGVsaFV5VDRjOW91TUZcbkU5WUV2YzdXVVlvbHkreXVzVW82Z0tLSE81QjhWdWJ0d0JGaWJoUW1QREJRSFJaVUhiYk1JZXRsb2ZUazE5MW9cbklBMS9Ka2tkV0RrR1FvSWYvTE5sdDV2M0ZILzluZ3hma2dzdE9BSXNVNzRlVkg0RmgvQkRlUm5UckxWN01QQy9cblFBSmxMY2txYll5Y0Myb29HYVRDT2c9PVxuLS0tLS1FTkQgUFJJVkFURSBLRVktLS0tLVxuIiwiY2xpZW50X2VtYWlsIjoiZmlyZWJhc2UtYWRtaW5zZGstNmMxdnpAam9iLXNlcnZlci0wODguaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJjbGllbnRfaWQiOiIxMTU3MDQ4NTUwMTIxNTg2NzE2ODEiLCJhdXRoX3VyaSI6Imh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbS9vL29hdXRoMi9hdXRoIiwidG9rZW5fdXJpIjoiaHR0cHM6Ly9vYXV0aDIuZ29vZ2xlYXBpcy5jb20vdG9rZW4iLCJhdXRoX3Byb3ZpZGVyX3g1MDlfY2VydF91cmwiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9vYXV0aDIvdjEvY2VydHMiLCJjbGllbnRfeDUwOV9jZXJ0X3VybCI6Imh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL3JvYm90L3YxL21ldGFkYXRhL3g1MDkvZmlyZWJhc2UtYWRtaW5zZGstNmMxdnolNDBqb2Itc2VydmVyLTA4OC5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVuaXZlcnNlX2RvbWFpbiI6Imdvb2dsZWFwaXMuY29tIn0='))
 
-let BASE_URL = decode('aHR0cHM6Ly9kYXRhYmFzZTA4OC1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20vcmFpeWFuMDg4Lw==')
+let BASE_URL = decode('aHR0cHM6Ly9qb2Itc2VydmVyLTA4OC1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20vcmFpeWFuMDg4Lw==')
+let STORAGE = decode('aHR0cHM6Ly9maXJlYmFzZXN0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vdjAvYi9qb2Itc2VydmVyLTA4OC5hcHBzcG90LmNvbS9vLw==')
 
 let WSS = decode('d3NzOi8vdHJ1c3RhcHJvaWFtLmRlOjEwMDA1Lw==')
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://database088-default-rtdb.firebaseio.com'
+    databaseURL: 'https://job-server-088-default-rtdb.firebaseio.com'
 })
 
 let app = express()
@@ -122,6 +124,8 @@ function connneckClient() {
     client.on('connect', function(conn) {
         if (mClient == null) {
             mClient = conn
+
+            mPrevJob = ''
     
             console.log('WebSocket Client Connected')
         
@@ -182,13 +186,32 @@ function connneckClient() {
 
 
 async function sendJob(job) {
-    try {
-        await axios.patch(BASE_URL+'mining/job.json', JSON.stringify(job), {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        })
-    } catch (error) {}
+    let mData = JSON.stringify(job)
+    if (mPrevJob != mData) {
+        try {
+            await axios.post(STORAGE+encodeURIComponent('mining/job.json'), '', {
+                headers: {
+                    'Content-Type':'base64/'+encode(mData)
+                },
+                maxBodyLength: Infinity,
+                maxContentLength: Infinity
+            })
+        } catch (error) {}
+
+        try {
+            await axios.patch(BASE_URL+'mining/job.json', mData, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+        } catch (error) {}
+
+        mPrevJob = mData
+    }
+}
+
+function encode(data) {
+    return Buffer.from(data).toString('base64')
 }
 
 function decode(data) {
